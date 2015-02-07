22c4bf4797b3c9ea83204dba4b2c32ae3698ef47
Oleh Sklyarenko <osklyarenko@lohika.com>
2015-02-07 19:58:32 +0200

    file_list api now fully supports unit and count params

--- server.rb
+++ server.rb
@@ -12,14 +12,25 @@ get '/' do
 	File.read("public/index.html")  
 end
 
+def calc_since(unit, count)	
+	case unit
+	when :day
+		count.to_i.days.ago.beginning_of_day
+	when :hour
+		count.to_i.hours.ago
+	when :week
+		count.to_i.weeks.ago
+	end
+end
+
 get '/api/files_list' do
 	content_type :json
 
-	unit = params[:unit]
-	count = params[:count]
+	unit = params[:unit] || 'day'
+	count = params[:count] || '1'
 
 	app = VisualMerge.new ['show']	
-	document = app.api_files_changed_since count.to_i.days.ago.beginning_of_day
+	document = app.api_files_changed_since calc_since(unit.to_sym, count.to_i)
 
 	document.to_json
 end
