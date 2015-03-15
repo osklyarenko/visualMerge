bcec7677099e60ade9cbe9177a9305a8b7eef335
Oleh Sklyarenko <osklyarenko@lohika.com>
2015-03-15 22:28:24 +0200

    Short file names are now unique across across the whole file set

--- visualMerge.rb
+++ visualMerge.rb
@@ -213,6 +213,60 @@ class VisualMerge
 			l_article[0] <=> r_article[0]			
 		end
 	end
+
+	def longer_file_path(full_file_path, short_file_path)
+		return full_file_path if full_file_path == short_file_path 
+
+		max_iter_count = 0
+		full_file_path.ascend {|_| max_iter_count = max_iter_count + 1 }
+
+
+		current_file_path = full_file_path
+		current_short_path = Pathname.new ''
+
+		for i in 1..max_iter_count
+			parent, current_basename = current_file_path.split
+			current_short_path = current_basename + current_short_path
+
+			current_file_path = parent
+			break if current_short_path.to_s.size > short_file_path.to_s.size		
+		end
+		
+		return current_short_path	
+	end
+
+
+	def unique_files(files)
+
+		files_grouped_by_short_name = files.group_by { |file| Pathname.new(file).basename.to_s }
+		
+		full_name2short_name = {}
+		unify_eventually(files_grouped_by_short_name).each_pair do |k, v|
+			full_name2short_name[v[0]] = k
+		end
+
+		return full_name2short_name
+	end
+
+	def unify_eventually(files_grouped_by_short_name)
+		uniques = files_grouped_by_short_name.select {|k, v| v.size == 1 }
+		non_uniques = files_grouped_by_short_name.select {|k, v| v.size > 1 }
+
+		return uniques if non_uniques.empty?
+
+		non_uniques.each_pair do |short_file_name, files|
+
+			files.each do |f|
+				longer_path = longer_file_path Pathname.new(f), Pathname.new(short_file_name)
+
+				uniques[longer_path.to_s] = [] unless uniques[longer_path.to_s]
+				uniques[longer_path.to_s] << f
+			end
+		end
+
+		return unify_eventually(uniques)
+	end
+
 	
 	def api_files_changed_since(since)
 		meta = git_files_changed_since '.', since
@@ -227,6 +281,8 @@ class VisualMerge
 
 		min_change = 3
 		max_change = 13
+		
+		unique_file_names_map = unique_files file_map.keys
 
 		file_map.each_pair do |file_name, changes|
 
@@ -264,6 +320,7 @@ class VisualMerge
 				:articles => articles,
 				:total => changes.size,
 				:name => file_path.basename.to_s,
+				:unique_name => unique_file_names_map[file_name],
 				:full_name => file_name,
 			}
 
